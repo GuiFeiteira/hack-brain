015da59916509c2f275751b144f8d4d7c1e7272d
Fur1GG <fur1gg.24@gmail.com>
2023-09-10 19:28:23 +0100

    Footer1

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Footer.css';
+import imagem1 from '../Recursos/25609.png'
+import imagem2 from '../Recursos/FHC_marca_Negativo_2.png'
+import imagem3 from '../Recursos/cm-covilha_2.png'
+import imagem4 from '../Recursos/logo2.png'
 
 const Footer = () => {
   return (
@@ -20,7 +24,10 @@ const Footer = () => {
                 info@exemplo.com
                 </td>
                 <td>
-                  olaaaa1
+                  <img className='imagem' src={imagem1} alt=''></img>
+                  <img className='imagem' src={imagem2} alt=''></img>
+                  <img className='imagem' src={imagem3} alt=''></img>
+                  <img className='imagem' src={imagem4} alt=''></img>
                 </td>
                 <td>
                   Intagrama
