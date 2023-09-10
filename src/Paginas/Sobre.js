edc62ff0fb3bbe91519015e97f06924356464741
GuiFeiteira <139795910+GuiFeiteira@users.noreply.github.com>
2023-09-10 19:54:11 +0100

    as

--- src/Paginas/Sobre.js
+++ src/Paginas/Sobre.js
@@ -4,6 +4,7 @@ import imagem1 from '../Recursos/25609.png'
 import imagem2 from '../Recursos/FHC_marca.png'
 import imagem3 from '../Recursos/covilha.png'
 import imagem4 from '../Recursos/logo_nucleo.png'
+import imagem5 from '../Recursos/logo_ubi_vprincipal.jpg'
 
 function Sobre() {
   return (
@@ -12,6 +13,7 @@ function Sobre() {
       <img className='imagem' src={imagem2} alt=''></img>
       <img className='imagem' src={imagem3} alt=''></img>
       <img className='imagem' src={imagem4} alt=''></img>
+      <img className='imagem' src={imagem5} alt=''></img>
     </div>
   )
 }
