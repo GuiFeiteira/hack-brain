02179cb752fb33c77106fe38b016413b653f2ed1
Fur1GG <fur1gg.24@gmail.com>
2023-09-11 17:11:14 +0100

    footer_done

--- src/Componentes/Footer.jsx
+++ src/Componentes/Footer.jsx
@@ -4,6 +4,8 @@ import imagem1 from '../Recursos/25609.png'
 import imagem2 from '../Recursos/FHC_marca_Negativo_2.png'
 import imagem3 from '../Recursos/cm-covilha_2.png'
 import imagem4 from '../Recursos/logo2.png'
+import imagem5 from '../Recursos/Amarelo__Branco.png'
+import imagem6 from '../Recursos/logo_ubi_branco.png'
 import insta from '../Recursos/instagram.png'
 import face from '../Recursos/facebook.png'
 import linkd from '../Recursos/linkedin.png'
@@ -16,9 +18,13 @@ const Footer = () => {
          <table className='footer_table'>
             <thead>
               <tr>
-                <th>Contactos</th>
-                <th></th>
-                <th>Redes Sociais</th>
+                <th>Contactos:</th>
+                <th>
+                  <div className='direitos'>
+                      <p>&copy; 2023 Direitos Reservados</p>
+                  </div>
+                </th>
+                <th>Redes Sociais:</th>
               </tr>
             </thead>
 
@@ -27,13 +33,15 @@ const Footer = () => {
                 <td>
                 hackthebrain@medubi.pt
                 </td>
-                <td>
+                <td className='logos_img'>
                   <img className='imagem' src={imagem1} alt=''></img>
                   <img className='imagem' src={imagem2} alt=''></img>
                   <img className='imagem' src={imagem3} alt=''></img>
-                  <img className='imagem' src={imagem4} alt=''></img>
+                  <img className='imagem_nucleo' src={imagem4} alt=''></img>
+                  <img className='imagem' src={imagem5} alt=''></img>
+                  <img className='imagem_ubi' src={imagem6} alt=''></img>
                 </td>
-                <td>
+                <td className='media_logos'>
                 <img className='imagem2' src={insta} alt=''></img>
                 <img className='imagem2' src={face} alt=''></img>
                 <img className='imagem2' src={linkd} alt=''></img>
@@ -43,9 +51,7 @@ const Footer = () => {
 
           </table>
 
-          <div className='direitos'>
-            <p>&copy; 2023 Direitos Reservados</p>
-          </div>
+          
     </footer>
   );
 };
