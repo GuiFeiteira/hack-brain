2f6459ab19e189580f3fd50b1ba3545a0275f327
Fur1GG <fur1gg.24@gmail.com>
2023-09-12 21:36:00 +0100

    mentores0

--- src/Paginas/Mentores.js
+++ src/Paginas/Mentores.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Mentores.css'
 
 function Mentores() {
+
+  const [name, setName] = useState('Mentor Name')
+  const [job, setJob] = useState('Mentor Job')
+  const [about, setAbout] = useState('Abouttttttttttttttttttttttttttttttttttttttttttttt')
+
+
   return (
-    <div>
-      
+    <div className='mentor_card'>
+      <div className='upper_container'>
+        <div className='image_container'>
+          <img src='' alt='' height="100px" width="100px"/>
+        </div>
+      </div>
+      <div className='lower_container'>
+        <h1>{name}</h1> 
+        <h4>{job}</h4>
+        <p>{about}</p>
+      </div>
     </div>
   )
 }
