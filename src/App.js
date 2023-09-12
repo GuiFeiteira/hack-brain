f09d8a059cc917ab7b8d171faba0b4cfa77b682e
GuiFeiteira <139795910+GuiFeiteira@users.noreply.github.com>
2023-09-12 21:07:28 +0100

    mentores

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ContactForm from './Paginas/Contacts';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Sobre from './Paginas/Sobre';
 import Footer from './Componentes/Footer';
-
+import Mentores from './Paginas/Mentores';
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/contactos" element={<ContactForm />} />
       <Route path="/sobre"  element={<Sobre/>} />
-
+      <Route path='/mentores' element={<Mentores/>} />
       </Routes>
       <Footer/>
       </Router>
---
+++ src/Paginas/Mentores.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Mentores() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Mentores
